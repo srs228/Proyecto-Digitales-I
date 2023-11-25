c6bae31fa23ed3004b64bd00a7780305a4ab5406
srs228 <sebasti200187@gmail.com>
2023-11-25 01:13:09 -0600

    Primeras adiciones al archivo izq_der, falta

--- Verilog/izq_der.v
+++ Verilog/izq_der.v
@@ -1,1 +1,18 @@
 //Recorrido Izquierda - Derecha
+
+//Comparador
+module Comparador
+(
+    input wire [7:0] a, b,
+    output wire w, z,
+);
+
+wire x;
+wire y;
+
+always @(a,b)
+    begin
+        assign w = ~(~a & b) & (a | b)
+        assign z = ~w
+    end
+endmodule // comparador
