618c62cdc0996a91e259136e6e0631bf4798988d
srs228 <sebasti200187@gmail.com>
2023-11-28 01:23:26 -0600

    Adicion archivo der_izq corregido y modificaciones en ambos testbenches

--- Verilog/Recorrido Derecha Izquierda/der_izq_tb1.v
+++ Verilog/Recorrido Derecha Izquierda/der_izq_tb1.v
@@ -1,27 +1,27 @@
-//Testbench Recorrido Izquierda a Derecha
+// Testbench Recorrido Derecha Izquierda
 
 `timescale 1 ns / 1 ps
 
-module izq_der_tb;
+module der_izq_tb;
 
     reg [7:0] a, b;
-    wire w, z;
+    wire z;
 
     localparam period = 10;
 
     //Dispositivo en prueba
-    Comparador DUT (.wordA(a), .wordB(b), .w(w), .z(z));
+    Comparador DUT (.wordA(a), .wordB(b), .z(z));
 
     initial
         begin
-            $dumpfile("izq_der_tb.vcd");
-            $dumpvars(1, izq_der_tb);
+            $dumpfile("der_izq_tb.vcd");
+            $dumpvars(1, der_izq_tb);
 
             a = 000;
             b = 000;
             #period;
 
-            if((w != 0) || (z != 1))
+            if(z != 1)
             begin
                 $display("Test failed for A0 = B0");
             end
@@ -30,7 +30,7 @@ module izq_der_tb;
             b = 001;
             #period
 
-            if((w != 1) || (z != 0))
+            if(z != 0)
             begin
                 $display("Test failed for A1 > B1");
             end
@@ -39,7 +39,7 @@ module izq_der_tb;
             b = 001;
             #period
 
-            if((w != 1) || (z != 0))
+            if(z != 0)
             begin
                 $display("Test failed for A2 > B2");
             end
@@ -48,7 +48,7 @@ module izq_der_tb;
             b = 001;
             #period
 
-            if((w != 0) || (z != 1))
+            if(z != 1)
             begin
                 $display("Test failed for A3 < B3");
             end
@@ -57,7 +57,7 @@ module izq_der_tb;
             b = 1000;
             #period
 
-            if((w != 1) || (z != 0))
+            if(z != 0)
             begin
                 $display("Test failed for A4 > B4");
             end
@@ -66,10 +66,10 @@ module izq_der_tb;
             b = 10000001;
             #period
 
-            if((w != 1) || (z != 0))
+            if(z != 0)
             begin
                 $display("Test failed for A5 > B5");
             end
 
         end
-endmodule // izq_der_tb
+endmodule // der_izq_tb
